999edba0ca05958272845f4260e24ccb1541151a
Der Meister <v.savchenko2408@gmail.com>
2026-01-27 01:13:32 +0100

    add Player

---
+++ include/player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player
+{
+    int _x;
+    int _y;
+    public:
+    Player();
+    int get_x();
+    int get_y();
+};
+
+#endif
---
+++ src/player.cpp
@@ -0,0 +1,16 @@
+#include "../include/player.h"
+
+Player::Player() :_x(5), _y(5)
+{
+
+}
+
+int Player::get_x()
+{
+    return _x;
+}
+
+int Player::get_y()
+{
+    return _y;
+}
