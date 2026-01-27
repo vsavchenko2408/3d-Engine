ea2d64772277f05db11c97326a12ce90ee4fa139
Der Meister <v.savchenko2408@gmail.com>
2026-01-27 01:07:27 +0100

    move Field() to .cpp

--- src/field.cpp
+++ src/field.cpp
@@ -1,1 +1,33 @@
 #include "../include/field.h"
+
+    Field::Field(): _map(_HEIGHT, std::vector<char>(_WIDTH))
+    {
+        for(int y = 0; y < _HEIGHT; y++)
+        {
+            for(int x = 0; x < _WIDTH; x++)
+            {
+                if(y == 0 || x == 0 || y == (_WIDTH-1) || x == (_HEIGHT-1))
+                {
+                    _map[x][y] = '#';
+                }
+                else
+                {
+                    _map[x][y] = ' ';
+                }
+            }
+        }
+    }
+
+
+    void Field::show_map()
+    {
+        for(int y = 0; y < _HEIGHT; y++)
+        {
+            for(int x = 0; x < _WIDTH; x++)
+            {
+                std::cout << _map[x][y];
+            }
+            std::cout << "\n";
+        }
+        
+    }
